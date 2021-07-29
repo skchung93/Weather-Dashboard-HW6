ad37f7f13a8a1b64a10b4b5cd9bfadf2090b681a
skchung93 <skchung93@gmail.comgit config --global user.email>
2021-07-28 22:01:17 -0700

    added the icons, made it a bit prettier, added search history population. Cannot figure out how to make the buttons work for the search history...

--- script.js
+++ script.js
@@ -11,9 +11,11 @@ var currentWind = document.querySelector('.current-wind');
 var currentHumidity = document.querySelector('.current-humidity');
 var currentUV = document.querySelector('.current-uv');
 var fiveDay = document.querySelector('.five-day');
+var searchHistory = document.querySelector('.search-history');
 var pastCity = [];
 //calling the function that handles the input and search button.
 userInput();
+
 
 
 //code to handle the input and search button!
@@ -26,7 +28,18 @@ function userInput(){
     currentCityWeather();
     pastCities();
     })
-}
+}        
+
+// function historicalSearch(){
+//     history = document.getElementsByTagName('li');
+//     history.addEventListener('click', function(event){
+//         event.preventDefault();
+//         citySearch = event.target.textContent;
+//         console.log(citySearch);
+        
+//     currentCityWeather();
+//     })
+// }
 
 //function for the current weather
 function currentCityWeather(){
@@ -58,12 +71,17 @@ function currentCityWeather(){
                 currentHumidity.innerHTML = "Humidity: " + data.current.humidity + "%";
                 currentWind.innerHTML = "Wind Speed: " + data.current.wind_speed + "MPH";
                 currentUV.innerHTML = "UV: " + data.current.uvi;
+                var currentIcon = document.createElement('img')
+                var icon1 = data.daily[0].weather[0].icon;
+                var iconImg1 = 'http://openweathermap.org/img/wn/' + icon1 + '.png';
+                currentIcon.setAttribute('src', iconImg1);
+                currentCity.append(currentIcon);
         
             //for loop to create the data for the 5 day forecast
             for(k=1; k<=5; k++){
                 var dayI = document.querySelector('.day-' + k);
                 var dates = document.createElement('p');
-                dates.classList.add('card-title', 'forecast');
+                dates.classList.add('card-title','dateheader' ,'forecast');
                 var maxTemps = document.createElement('p');
                 maxTemps.classList.add('card-text', 'forecast');
                 var minTemps = document.createElement('p');
@@ -72,12 +90,16 @@ function currentCityWeather(){
                 hums.classList.add('card-text', 'forecast');
                 var wins = document.createElement('p');
                 wins.classList.add('card-text', 'forecast');
+                var icons = document.createElement('img')
+                // icons.classList.add('card-text', 'forecast');
 
                 var date = moment().add(k, 'days').format('M/D/YYYY');
                 var maxTemp = data.daily[k].temp.max;
                 var minTemp = data.daily[k].temp.min;
                 var humidity = data.daily[k].humidity;
                 var wind = data.daily[k].wind_speed;
+                var iconNum = data.daily[k].weather[0].icon;
+                var iconImg = 'http://openweathermap.org/img/wn/' + iconNum + '.png';
                 
 
                 dates.innerHTML = date;
@@ -85,8 +107,11 @@ function currentCityWeather(){
                 minTemps.innerHTML = "Min Temp: " + minTemp + "\xB0F";
                 hums.innerHTML = "Humidity: " + humidity;
                 wins.innerHTML = "Wind Speed: " + wind + "MPH";
+                icons.setAttribute('src', iconImg);
 
+            //appending the data to the five day forecast
                 dayI.append(dates);
+                dayI.append(icons);
                 dayI.append(maxTemps);
                 dayI.append(minTemps);
                 dayI.append(hums);
@@ -99,8 +124,26 @@ function currentCityWeather(){
 }
 
 
-//function to save inputs?
+//function to save inputs and create buttons?
 function pastCities(){
+    $('ul').empty(); 
     pastCity.push(city)
     localStorage.setItem('pastCities', JSON.stringify(pastCity))
-}
+//for loop to create search history cities
+    for (i=0; i<pastCity.length; i++){
+        var cityHistory = document.createElement('li');
+        cityHistory.classList.add('past-city');
+        cityHistory.setAttribute('type', 'button');
+        cityHistory.innerHTML = pastCity[i];
+        searchHistory.prepend(cityHistory);
+    }
+}    
+
+// history.addEventListener('click', pastCities, function(event){
+//     event.preventDefault();
+//     citySearch = event.target.textContent;
+//     console.log(citySearch);
+    
+// currentCityWeather();
+// })
+
